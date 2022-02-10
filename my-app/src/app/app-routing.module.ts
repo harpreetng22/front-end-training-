079372ebe91d16ee64cf8ebe1e2ab28bdf130eab
Harpreet Singh14 <harpreet.singh14@ibm.com>
2022-02-10 14:58:16 +0530

    10-2-2022

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'main',component:TestComponent},{path:'home',component:HomeComponent},{path:'about',component:AboutComponent},{path:'contact',component:ContactComponent},{path:'login',component:LoginComponent},
+{path:'',redirectTo:'home',pathMatch:'full'},{path:'**',component:ErrorComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { DataService } from './data.service';
 import { LoginComponent } from './login/login/login.component';
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent,LoginComponent, TestComponent
+    AppComponent,LoginComponent, TestComponent, HomeComponent, AboutComponent, ContactComponent, FooterComponent, ErrorComponent
   ],
   imports: [
     BrowserModule,
